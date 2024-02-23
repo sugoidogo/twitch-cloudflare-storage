475017c07607f83435774a3ecb247424d6d3cb42
josephsmendoza <josephsmendoza@gmail.com>
2024-02-23 11:54:52 -0800

    print more detailed error information on the command line

--- src/index.js
+++ src/index.js
@@ -25,114 +25,119 @@ function makeResponse(body = undefined, init = undefined) {
 
 export default {
 	async fetch(request, env) {
-		if (request.method === 'OPTIONS') {
-			return makeResponse()
-		}
-
-		if (!request.headers.has('authorization')) {
-			return makeResponse('{"status":401,"message":"missing authorization token"}',
-				{ status: 401 }
-			)
-		}
-		let response = await fetch('https://id.twitch.tv/oauth2/validate', {
-			headers: {
-				'authorization': request.headers.get('authorization')
+		try {
+			if (request.method === 'OPTIONS') {
+				return makeResponse()
 			}
-		})
-		if (!response.ok) {
-			return response
-		}
-		response = await response.json()
-		const requestRoot = Path.join(response.user_id, response.client_id)
-		const pathname = new URL(request.url).pathname
-		const objectName = Path.join(requestRoot, pathname)
-		if (objectName.indexOf(requestRoot) !== 0) {
-			return makeResponse(undefined, { status: 400 })
-		}
 
-		console.log(`${request.method} object ${objectName}: ${request.url}`)
-
-		if (request.method === "GET") {
-
-			if (pathname.endsWith('/')) {
-				console.log('getting object listing')
-				const listing = await env.storage.list({ prefix: objectName })
-				console.log()
-				const response = []
-				for (const object of listing.objects) {
-					let key = object.key.slice(objectName.length).split('/')[0]
-					response.push(key)
+			if (!request.headers.has('authorization')) {
+				return makeResponse('{"status":401,"message":"missing authorization token"}',
+					{ status: 401 }
+				)
+			}
+			let response = await fetch('https://id.twitch.tv/oauth2/validate', {
+				headers: {
+					'authorization': request.headers.get('authorization')
 				}
-				return makeResponse(JSON.stringify(response), {
+			})
+			if (!response.ok) {
+				return response
+			}
+			response = await response.json()
+			const requestRoot = Path.join(response.user_id, response.client_id)
+			const pathname = new URL(request.url).pathname
+			const objectName = Path.join(requestRoot, pathname)
+			if (objectName.indexOf(requestRoot) !== 0) {
+				return makeResponse(undefined, { status: 400 })
+			}
+
+			console.log(`${request.method} object ${objectName}: ${request.url}`)
+
+			if (request.method === "GET") {
+
+				if (pathname.endsWith('/')) {
+					console.log('getting object listing')
+					const listing = await env.storage.list({ prefix: objectName })
+					console.log()
+					const response = []
+					for (const object of listing.objects) {
+						let key = object.key.slice(objectName.length).split('/')[0]
+						response.push(key)
+					}
+					return makeResponse(JSON.stringify(response), {
+						headers: {
+							'content-type': 'application/json; charset=UTF-8',
+						}
+					})
+				}
+
+				const object = await env.storage.get(objectName, {
+					range: request.headers,
+					onlyIf: request.headers
+				})
+
+				if (object === null) {
+					return objectNotFound(objectName)
+				}
+
+				const headers = new Headers()
+				object.writeHttpMetadata(headers)
+				headers.set("etag", object.httpEtag)
+				if (object.range) {
+					headers.set(
+						"content-range",
+						`bytes ${object.range.offset}-${object.range.end ??
+						object.size - 1}/${object.size}`
+					)
+				}
+				const status = object.body
+					? request.headers.get("range") !== null
+						? 206
+						: 200
+					: 304
+				return makeResponse(object.body, {
+					headers,
+					status
+				})
+			}
+
+			if (request.method === "HEAD") {
+				const object = await env.storage.head(objectName)
+
+				if (object === null) {
+					return objectNotFound(objectName)
+				}
+
+				const headers = new Headers()
+				object.writeHttpMetadata(headers)
+				headers.set("etag", object.httpEtag)
+				return makeResponse(null, {
+					headers
+				})
+			}
+
+			if (request.method === "PUT" || request.method == "POST") {
+				const object = await env.storage.put(objectName, request.body, {
+					httpMetadata: request.headers
+				})
+				return makeResponse(null, {
 					headers: {
-						'content-type': 'application/json; charset=UTF-8',
+						etag: object.httpEtag
 					}
 				})
 			}
 
-			const object = await env.storage.get(objectName, {
-				range: request.headers,
-				onlyIf: request.headers
-			})
-
-			if (object === null) {
-				return objectNotFound(objectName)
+			if (request.method === "DELETE") {
+				await env.storage.delete(url.pathname.slice(1))
+				return makeResponse()
 			}
 
-			const headers = new Headers()
-			object.writeHttpMetadata(headers)
-			headers.set("etag", object.httpEtag)
-			if (object.range) {
-				headers.set(
-					"content-range",
-					`bytes ${object.range.offset}-${object.range.end ??
-					object.size - 1}/${object.size}`
-				)
-			}
-			const status = object.body
-				? request.headers.get("range") !== null
-					? 206
-					: 200
-				: 304
-			return makeResponse(object.body, {
-				headers,
-				status
+			return makeResponse(`Unsupported method`, {
+				status: 400
 			})
+		} catch (e) {
+			console.error(e)
+			throw e
 		}
-
-		if (request.method === "HEAD") {
-			const object = await env.storage.head(objectName)
-
-			if (object === null) {
-				return objectNotFound(objectName)
-			}
-
-			const headers = new Headers()
-			object.writeHttpMetadata(headers)
-			headers.set("etag", object.httpEtag)
-			return makeResponse(null, {
-				headers
-			})
-		}
-
-		if (request.method === "PUT" || request.method == "POST") {
-			const object = await env.storage.put(objectName, request.body, {
-				httpMetadata: request.headers
-			})
-			return makeResponse(null, {
-				headers: {
-					etag: object.httpEtag
-				}
-			})
-		}
-
-		if (request.method === "DELETE") {
-			await env.storage.delete(url.pathname.slice(1))
-			return makeResponse()
-		}
-
-		return makeResponse(`Unsupported method`, {
-			status: 400
-		})
 	}
 }
